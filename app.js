1b9afd7792cc78861158cadee1cc9b82f7128acc
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-07-06 16:07:16 -0500

    Completed react-components solo work

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+   {props.items.map(item =>
+     <GroceryListItem item={item} />
+   )}
+ </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+        done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal '
+    };
+    return <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>;
+  }
+}
+
+ReactDOM.render(<GroceryList items={['Milk', 'Eggs', 'Orange Juice']}/>, document.getElementById("app"));
